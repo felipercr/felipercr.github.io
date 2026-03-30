0f02ad47c56449dedff2f4b675a6ff33e323d8f5
Felipe Reis <felipercr.eng@gmail.com>
2026-03-30 09:52:51 +0200

    neko test

---
+++ _scripts/neko.js
@@ -0,0 +1,35 @@
+var neko = document.createElement("div");
+neko.id = "oneko";
+neko.style.width = "32px";
+neko.style.height = "32px";
+neko.style.position = "fixed";
+neko.style.pointerEvents = "none";
+neko.style.backgroundImage = "url('https://raw.githubusercontent.com/adryd325/oneko/main/oneko.gif')";
+neko.style.imageRendering = "pixelated";
+neko.style.left = "16px";
+neko.style.top = "16px";
+neko.style.zIndex = "1000";
+document.body.appendChild(neko);
+
+var x = 16;
+var y = 16;
+var targetX = 16;
+var targetY = 16;
+var nekoSpeed = 0.05;
+
+function updateNeko() {
+  var dx = targetX - x;
+  var dy = targetY - y;
+  x += dx * nekoSpeed;
+  y += dy * nekoSpeed;
+  neko.style.left = x + "px";
+  neko.style.top = y + "px";
+  requestAnimationFrame(updateNeko);
+}
+
+document.addEventListener("mousemove", function(e) {
+  targetX = e.clientX - 16;
+  targetY = e.clientY - 16;
+});
+
+updateNeko();
