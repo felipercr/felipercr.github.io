0af2cb7e64ecf8b911758155b5125928037a9b06
felipercr <felipercr.eng@gmail.com>
2026-04-21 20:51:44 +0000

    Deploying to gh-pages from @ felipercr/felipercr.github.io@a42530428746b27a78afaf705e368ff892cb2225 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-publications",
           title: "Publications",
           description: "My publications.",
